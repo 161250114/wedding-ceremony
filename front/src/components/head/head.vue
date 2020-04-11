ba4ec81303240a556030d74fc92a41c7dd8c7229
161250114 <1422145612@qq.com>
2020-04-11 19:09:32 +0800

    编写head

--- front/src/components/head/head.vue
+++ front/src/components/head/head.vue
@@ -1,10 +1,46 @@
 <template>
-  <div class="head">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">首页</el-menu-item>
-      <el-menu-item index="2">团购</el-menu-item>
-      <el-menu-item index="3">管理</el-menu-item>
-    </el-menu>
+  <div>
+    <el-header class="header">
+      <el-row>
+        <el-col :span="2">
+          &nbsp;
+        </el-col>
+        <a href="./">
+          <el-col :span="2">
+            <el-avatar shape="square" :size="55" fit="fill" src="../../../static/timg.jpg"></el-avatar>
+          </el-col>
+          <el-col :span="3">
+            <img src="../../../static/title.png" class="title-img">
+          </el-col>
+        </a>
+        <el-col :span="13">
+          &nbsp;
+        </el-col>
+          <el-col :span="3" v-show="!isLogin">
+            <el-menu :default-active="activeIndex" class="header" mode="horizontal" @select="handleSelect">
+              <el-menu-item index="1">登录</el-menu-item>
+              <el-menu-item index="2">注册</el-menu-item>
+            </el-menu>
+          </el-col>
+          <el-col :span="3" v-show="isLogin"> 
+            <h3>欢迎您，{{username}}&nbsp;
+              <el-dropdown @command="handleCommand">
+                <span class="el-dropdown-link">
+                  <i class="el-icon-user-solid"></i>
+                </span>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item command="1">功能1</el-dropdown-item>
+                    <el-dropdown-item command="2">功能2</el-dropdown-item>
+                    <el-dropdown-item command="3">功能3</el-dropdown-item>
+                    <el-dropdown-item command="4">功能4</el-dropdown-item>
+                    <el-dropdown-item divided command="quit">退出登录</el-dropdown-item>
+                  </el-dropdown-menu>
+              </el-dropdown>
+            </h3>
+            
+          </el-col>
+      </el-row>
+    </el-header>
   </div>
 </template>
 
@@ -18,22 +54,35 @@
     },
     data(){
       return{
-        activeIndex: '1'
+        activeIndex:3,
+        isLogin:false,
+        username:"pikaqiu"
       }
     },
     created(){
-      this.activeIndex = this.index
+
     },
     methods: {
       handleSelect(key) {
         switch(key){
           case  '1':
-            this.$router.push("/")
+            let app=this;
+            app.isLogin=!app.isLogin;
+            // this.$router.push("/login")
             break;
-          case  '3':
-            this.$router.push("/login")
+          case  '2':
+            // this.$router.push("/register")
             break;
 
+        }
+      },
+      handleCommand(command){
+        let app=this;
+        switch(command){
+          case 'quit':
+            app.isLogin=!app.isLogin;
+            app.activeIndex=3;
+            break;
         }
       }
     }
@@ -41,5 +90,14 @@
 </script>
 
 <style scoped>
-
+  .header{
+    background: #f6f6f6;
+  }
+  .title-img{
+    height: 55px;
+  }
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
 </style>
